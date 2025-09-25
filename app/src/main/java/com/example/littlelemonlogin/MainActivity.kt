f2b7631cac08187e09f63530edd901730dbbd9d6
lsnsky <lsnsky.de@gmail.com>
2025-09-25 14:44:17 +0200

    Login Screen with logo, text fields and button created

---
+++ app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.littlelemonlogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.littlelemonlogin.ui.theme.LittleLemonLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LittleLemonLoginTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(){
+    Column (Modifier.fillMaxSize()
+        , verticalArrangement = Arrangement.Center
+        , horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painterResource(R.drawable.logo_ll), "Logo Image")
+        TextField(onValueChange = {}, value = "", label={Text(text="Username")})
+        TextField(onValueChange = {}, value = "", label={Text(text="Password")})
+        Button({/*TODO*/}
+        , colors = ButtonDefaults.buttonColors(Color(0xFF495E57))){
+            Text(text="Login", color = Color(0xFFEDEFEF))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LittleLemonLoginTheme {
+        LoginScreen()
+    }
+}
